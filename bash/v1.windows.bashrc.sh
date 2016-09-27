6436c2d8e393210e39a22c15e51d234f4a7c7c5b
Andrew Smith <andrew@andrew.codes>
2016-09-27 08:20:49 -0400

    Correct empty function bash errors
    - need to implement dev license
    - need to implement dbnuke to demo data

--- bash/v1.windows.bashrc.sh
+++ bash/v1.windows.bashrc.sh
@@ -10,42 +10,42 @@ function v1() {
     `$command`
 }
 
-function v1l ()
-{
-
-}
+# function v1l ()
+# {
+#     v1 license:dev
+# }
 
 function v1up ()
 {
     v1 upgradedb
 }
 
-v1nuke ()
-{
-  # Version="${1:-15.1}"
-  # if [ -f $DemoData ]; then
-  #   DemoData=$DEV_HOME/utilities/DemoData/${Version}/DemoData.bak
-  #   DemoDataWinPath=$(echo $DemoData | sed -e "s/\///" | sed -e "s/\//\\\/g" | sed -e "s/\\\/:\\\/")
-  #   sql="
-  #     USE [master]
-  #     RESTORE DATABASE [VersionOne] FROM  DISK = N'$DemoDataWinPath' WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 5
-  #     GO
-  #   "
-  #   iisreset
-  #   sqlcmd -S localhost -U sa -P dev -Q "$sql"
-  #   sqlcmd -S localhost -U sa -P dev -Q "
-  #     USE [VersionOne]
-  #     GO
-  #     CREATE USER [pub] FOR LOGIN [pub] WITH DEFAULT_SCHEMA=[dbo]
-  #     GO
-  #     USE [VersionOne]
-  #     GO
-  #     ALTER ROLE [db_owner] ADD MEMBER [pub]
-  #     GO
-  #   "
-  # fi
-  # currentDir=$PWD
-  # cd $REPO_HOME/Core
-  # upgrade
-  # cd $currentDir
-}
+# v1nuke ()
+# {
+#   # Version="${1:-15.1}"
+#   # if [ -f $DemoData ]; then
+#   #   DemoData=$DEV_HOME/utilities/DemoData/${Version}/DemoData.bak
+#   #   DemoDataWinPath=$(echo $DemoData | sed -e "s/\///" | sed -e "s/\//\\\/g" | sed -e "s/\\\/:\\\/")
+#   #   sql="
+#   #     USE [master]
+#   #     RESTORE DATABASE [VersionOne] FROM  DISK = N'$DemoDataWinPath' WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 5
+#   #     GO
+#   #   "
+#   #   iisreset
+#   #   sqlcmd -S localhost -U sa -P dev -Q "$sql"
+#   #   sqlcmd -S localhost -U sa -P dev -Q "
+#   #     USE [VersionOne]
+#   #     GO
+#   #     CREATE USER [pub] FOR LOGIN [pub] WITH DEFAULT_SCHEMA=[dbo]
+#   #     GO
+#   #     USE [VersionOne]
+#   #     GO
+#   #     ALTER ROLE [db_owner] ADD MEMBER [pub]
+#   #     GO
+#   #   "
+#   # fi
+#   # currentDir=$PWD
+#   # cd $REPO_HOME/Core
+#   # upgrade
+#   # cd $currentDir
+# }
