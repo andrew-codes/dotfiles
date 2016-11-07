2f79913bea5e7db8b9fb75c922e5c54c11eceba5
Andrew Smith <andrew@andrew.codes>
2016-11-07 17:32:04 -0500

    Include alias for Beyond Compare

--- dotfiles/bash/windows.bashrc.sh
+++ dotfiles/bash/windows.bashrc.sh
@@ -5,4 +5,5 @@ function recycle ()
   powershell -Command "& { ([ADSI] 'IIS://localhost/W3SVC/AppPools/ASP.NET v4.0').psbase.invoke('recycle') }"
 }
 
+alias bc="/C/Program\ Files/Beyond\ Compare\ 4/BCompare.exe"
 PATH=$PATH:$DEVELOPER_UTILITIES_HOME/curl-7.50.3/usr/bin
