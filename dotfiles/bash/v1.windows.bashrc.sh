884f0d33a2b3216b0bf556512d5c79020a585065
Andrew Smith <andrew@andrew.codes>
2016-11-07 17:32:04 -0500

    v1nuke downloads demo data from artifactory and restores database LifeCycle-{Version}

--- dotfiles/bash/v1.windows.bashrc.sh
+++ dotfiles/bash/v1.windows.bashrc.sh
@@ -23,32 +23,34 @@ function v1up ()
     v1 upgradedb
 }
 
-# v1nuke ()
-# {
-#   # Version="${1:-15.1}"
-#   # if [ -f $DemoData ]; then
-#   #   DemoData=$DEV_HOME/utilities/DemoData/${Version}/DemoData.bak
-#   #   DemoDataWinPath=$(echo $DemoData | sed -e "s/\///" | sed -e "s/\//\\\/g" | sed -e "s/\\\/:\\\/")
-#   #   sql="
-#   #     USE [master]
-#   #     RESTORE DATABASE [VersionOne] FROM  DISK = N'$DemoDataWinPath' WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 5
-#   #     GO
-#   #   "
-#   #   iisreset
-#   #   sqlcmd -S localhost -U sa -P dev -Q "$sql"
-#   #   sqlcmd -S localhost -U sa -P dev -Q "
-#   #     USE [VersionOne]
-#   #     GO
-#   #     CREATE USER [pub] FOR LOGIN [pub] WITH DEFAULT_SCHEMA=[dbo]
-#   #     GO
-#   #     USE [VersionOne]
-#   #     GO
-#   #     ALTER ROLE [db_owner] ADD MEMBER [pub]
-#   #     GO
-#   #   "
-#   # fi
-#   # currentDir=$PWD
-#   # cd $REPO_HOME/Core
-#   # upgrade
-#   # cd $currentDir
-# }
+v1nuke ()
+{
+  DemoDataDirectory=$DEV_HOME/Data/Demo
+
+  Version="${1:-16.1}"
+  DemoDataFile=$DemoDataDirectory/$Version/V1Demo-$Version.0.0.bak
+  if [ ! -f $DemoDataDirectory/$Version ]; then
+    mkdir -p $DemoDataDirectory/$Version
+  fi
+  if [ ! -f $DemoDataFile ]; then
+    curl http://artifactory.dev.corp.versionone.net/artifactory/lifecycle-demodata/$Version/V1Demo-$Version.0.0.bak --output $DemoDataFile
+  fi
+  DemoDataWinPath=$(echo $DemoDataFile | sed -e "s/\///" | sed -e "s/\//\\\/g" | sed -e "s/\\\/:\\\/")
+    sql="
+      USE [master]
+      RESTORE DATABASE [LifeCycle-$Version] FROM  DISK = N'$DemoDataWinPath' WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 5
+      GO
+    "
+    iisreset
+    sqlcmd -S localhost -U sa -P dev -Q "$sql"
+    sqlcmd -S localhost -U sa -P dev -Q "
+      USE [LifeCycle-$Version]
+      GO
+      CREATE USER [pub] FOR LOGIN [pub] WITH DEFAULT_SCHEMA=[dbo]
+      GO
+      USE [LifeCycle-$Version]
+      GO
+      ALTER ROLE [db_owner] ADD MEMBER [pub]
+      GO
+    "
+}
